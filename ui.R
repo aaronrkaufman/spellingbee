07d10b0e733775f522ccaa06f8cc1ef9a6ca5d40
Aaron Kaufman <ak8096@nyu.edu>
2020-05-13 15:42:51 -0400

    rearrange layout

--- ui.R
+++ ui.R
@@ -1,26 +1,26 @@
 fluidPage(
   shinyjs::useShinyjs(),
-  verticalLayout(
-    titlePanel("R Shiny Spelling Bee"),
-    column(8,
-      plotOutput("plot1", height=600, width=600)
-    ),
-    column(4,
-      verbatimTextOutput("score_of_max"),
-      verbatimTextOutput("wordtable")     
-    ),
-    textOutput("message"),
-    hr(),
-    wellPanel(
+  titlePanel("R Shiny Spelling Bee"),
+  sidebarLayout(
+    
+    sidebarPanel(
       tags$head(tags$script(src = "enter_button.js")), 
-#      sliderInput("n", "Number of points", 10, 200,
-#                  value = 50, step = 10)
+      #      sliderInput("n", "Number of points", 10, 200,
+      #                  value = 50, step = 10)
       textInput("guess", "Word", placeholder = "Your Guess Here"),
-      actionButton("goButton", "Submit")#,
+      actionButton("goButton", "Submit"),
+      br(),
+      br(),
+      verbatimTextOutput("score_of_max"),
+      verbatimTextOutput("wordtable"),
+      textOutput("message")
+    ),
+    mainPanel(
+        plotOutput("plot1", height=600, width=600)
+    )
       #br(),
       #br(),
       #actionButton("reshuffle", "Reshuffle")
-    )
   )
 )
 
