5dbd9ee73edcd8b8ef7bfb7b37fc61bae4ec13af
Aaron Kaufman <ak8096@nyu.edu>
2020-05-13 15:55:39 -0400

    rearrange layout some more

--- ui.R
+++ ui.R
@@ -7,14 +7,15 @@ fluidPage(
       tags$head(tags$script(src = "enter_button.js")), 
       #      sliderInput("n", "Number of points", 10, 200,
       #                  value = 50, step = 10)
-      textInput("guess", "Word", placeholder = "Your Guess Here"),
+      h5("Your word must be at least 4 letters long, and contain the center letter."),
+      textInput("guess", "Enter Words Here", placeholder = "Your Guess Here"),
       actionButton("goButton", "Submit"),
       br(),
       br(),
+      textOutput("message"),
       verbatimTextOutput("score_of_max"),
-      verbatimTextOutput("wordtable"),
-      textOutput("message")
-    ),
+      verbatimTextOutput("wordtable")
+      ),
     mainPanel(
         plotOutput("plot1", height=600, width=600)
     )
